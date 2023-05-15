890c05a1cc56e00a2a420a413d45c0f6be6401a4
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-05-15 20:08:13 +0200

    Add react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './App.scss';
 import Guestlist from './Guestlist';
 
--- src/Guestlist.js
+++ src/Guestlist.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function Guestlist() {
   const [firstName, setFirstName] = useState('');
